92ef4ffd8c9496ea0a3ea84c5f1eb4b6e18d3103
Aryan Mahindra <62005619+Aryan181@users.noreply.github.com>
2023-06-04 11:37:18 -0700

    files

--- out/extension.js
+++ out/extension.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deactivate = exports.activate = void 0;
+exports.activate = void 0;
 const openai_1 = require("openai");
+const fs = require("fs");
+const path = require("path");
 class ChatGPTClient {
     constructor() {
         const configuration = new openai_1.Configuration({
@@ -43,23 +45,23 @@ const vscode = require("vscode");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('bunyan.generate', async () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             const document = editor.document;
             const text = document.getText();
-            // console.log(text);
-            // Insert a random character every two lines
+            // read text from style.txt
+            let styleText;
+            try {
+                styleText = fs.readFileSync(path.resolve(__dirname, 'style.txt'), 'utf8');
+            }
+            catch (error) {
+                console.error(`Error reading style.txt: ${error}`);
+                return;
+            }
             let newText = '';
-            //Make call to gpt-4 
             const chatGPTClient = new ChatGPTClient();
-            // Create a new chatGPT message
             const chatGPTMessage = {
                 role: 'user',
                 content: text,
@@ -68,16 +70,15 @@ function activate(context) {
                 role: 'assistant',
                 content: 'Make changes only when necessary. Respond with only code, no natural language.'
             };
+            // added style text to the chatGPTMessage2
             const chatGPTMessage2 = {
                 role: 'user',
-                content: 'Given contents of a code file, return the revised code that adheres to the guide. Make changes only when necessary. Respond with only code, no natural language.'
+                content: `Given contents of a code and the style guide file, return the revised code following style guide: ${styleText}. Only code response`
             };
-            // Create a new chatGPT request
             const chatGPTRequest = {
                 model: 'gpt-4-32k',
                 messages: [instructionMessage, chatGPTMessage, chatGPTMessage2],
             };
-            // Send the chatGPT request
             await chatGPTClient
                 .respond([chatGPTMessage])
                 .then(response => {
@@ -92,7 +93,6 @@ function activate(context) {
                 console.log(error);
                 newText = 'The bot did not respond. Please try again later.';
             });
-            // Replace the text in the editor with the new text
             editor.edit(editBuilder => {
                 const range = new vscode.Range(document.positionAt(0), document.positionAt(text.length));
                 editBuilder.replace(range, newText);
@@ -102,7 +102,4 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// This method is called when your extension is deactivated
-function deactivate() { }
-exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,9 @@ import {
     ChatCompletionRequestMessageRoleEnum
 } from 'openai';
 
+import * as fs from 'fs';
+import * as path from 'path';
+
 class ChatGPTClient {
     private openAI: OpenAIApi;
     constructor() {
@@ -49,81 +52,72 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension is now active!');
+    let disposable = vscode.commands.registerCommand('bunyan.generate', async () => {
+        const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bunyan.generate', async () => {
+        if (editor) {
+            const document = editor.document;
+            const text = document.getText();
+            
+            // read text from style.txt
+            let styleText;
+            try {
+                styleText = fs.readFileSync(path.resolve(__dirname, 'style.txt'), 'utf8');
+            } catch (error) {
+                console.error(`Error reading style.txt: ${error}`);
+                return;
+            }
 
-		const editor = vscode.window.activeTextEditor;
+            let newText = '';
+            const chatGPTClient = new ChatGPTClient();
 
-		if (editor) {
-			const document = editor.document;
-			const text = document.getText();
+            const chatGPTMessage: ChatCompletionRequestMessage = {
+                role: 'user',
+                content: text,
+            };
 
-			// console.log(text);
+            const instructionMessage: ChatCompletionRequestMessage = {
+                role: 'assistant',
+                content: 'Make changes only when necessary. Respond with only code, no natural language.'
+            };
 
-			// Insert a random character every two lines
-			let newText = '';
+            // added style text to the chatGPTMessage2
+            const chatGPTMessage2: ChatCompletionRequestMessage = {
+                role: 'user',
+                content: `Given contents of a code and the style guide file, return the revised code following style guide: ${styleText}. Only code response`
+            };
 
-			//Make call to gpt-4 
-			const chatGPTClient = new ChatGPTClient();
+            const chatGPTRequest: CreateChatCompletionRequest = {
+                model: 'gpt-4-32k',
+                messages: [instructionMessage, chatGPTMessage, chatGPTMessage2],
+            };
 
-			// Create a new chatGPT message
-			const chatGPTMessage: ChatCompletionRequestMessage = {
-				role: 'user',
-				content: text,
-			};
+            await chatGPTClient
+                .respond([chatGPTMessage])
+                .then(response => {
+                    console.log("response", response);
+                    if (!response.text) {
+                        newText = 'The bot did not respond. Please try again later.';
+                        return;
+                    }
+                    newText = response.text;
+                })
+                .catch(error => {
+                    console.log(error);
+                    newText = 'The bot did not respond. Please try again later.';
+                });
 
-			const instructionMessage: ChatCompletionRequestMessage = {
-				role: 'assistant',
-				content: 'Make changes only when necessary. Respond with only code, no natural language.'
-			};
+            editor.edit(editBuilder => {
+                const range = new vscode.Range(
+                    document.positionAt(0),
+                    document.positionAt(text.length)
+                );
+                editBuilder.replace(range, newText);
+            });
+        }
+    });
 
-			const chatGPTMessage2: ChatCompletionRequestMessage = {
-				role: 'user',
-				content: 'Given contents of a code file, return the revised code that adheres to the guide. Make changes only when necessary. Respond with only code, no natural language.'
-			};
-
-			// Create a new chatGPT request
-			const chatGPTRequest: CreateChatCompletionRequest = {
-				model: 'gpt-4-32k',
-				messages: [instructionMessage, chatGPTMessage, chatGPTMessage2],
-			};
-
-			// Send the chatGPT request
-			await chatGPTClient
-				.respond([chatGPTMessage])
-				.then(response => {
-					console.log("response", response);
-					if (!response.text) {
-						newText = 'The bot did not respond. Please try again later.';
-						return;
-					}
-					newText = response.text;
-				})
-				.catch(error => {
-					console.log(error);
-					newText = 'The bot did not respond. Please try again later.';
-				});
-
-			// Replace the text in the editor with the new text
-			editor.edit(editBuilder => {
-				const range = new vscode.Range(
-					document.positionAt(0),
-					document.positionAt(text.length)
-				);
-				editBuilder.replace(range, newText);
-			});
-		}
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
